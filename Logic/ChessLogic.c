2a751806f6dcaecba1f90a1dfa03b1a1a13219fb
Eragon189 <144192904+Eragon189@users.noreply.github.com>
2025-07-08 12:15:44 -0700

    Added Arrays to repersent each peace

--- Logic/ChessLogic.c
+++ Logic/ChessLogic.c
@@ -1,1 +1,17 @@
-int WhiteKing[4] = [0,0,0,]
+//Defining all peaces as array in form  ["file","rank","","pic path"] 
+char WhiteKing[4] = ["0","0","0","Recorces/White/LightKing.png"]
+char WhiteBishop[4] = ["0","0","0","Recorces/White/LightBishop.png"]
+char WhiteKnight[4] = ["0","0","0","Recorces/White/LightKnight.png"]
+char WhitePawn[4] = ["0","0","0","Recorces/White/LightPawn.png"]
+char WhiteQueen[4] = ["0","0","0","Recorces/White/LightQueen.png"]
+char WhiteRook[4] = ["0","0","0","Recorces/White/LightRook.png"]
+char BlackKing[4] = ["0","0","0","Recorces/Black/DarkKing.png"]
+char BlackBishop[4] = ["0","0","0","Recorces/Black/DarkBishop.png"]
+char BlackKnight[4] = ["0","0","0","Recorces/Black/DarkKnight.png"]
+char BlackPawn[4] = ["0","0","0","Recorces/Black/DarkPawn.png"]
+char BlackQueen[4] = ["0","0","0","Recorces/Black/DarkQueen.png"]
+char BlackRook[4] = ["0","0","0","Recorces/Black/DarkRook.png"]
+
+
+
+
